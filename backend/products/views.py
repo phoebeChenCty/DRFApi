f24cef8b8e0bb90e4e8129a76956e80fbab68d2c
PhoebeCTY <phoebechen1996@gmail.com>
2022-07-17 19:21:33 -0400

    Using Function Based Views

--- backend/products/views.py
+++ backend/products/views.py
@@ -2,6 +2,10 @@ from sqlite3 import connect
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
@@ -25,7 +29,7 @@ class ProductCreateAPIView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         title = serializer.validated_data.get('title')
-        content = serializer.validated_data.get('content')
+        content = serializer.validated_data.get('content') or None
         if content is None:
             content = title
         serializer.save(content=content)
@@ -51,3 +55,32 @@ class ProductListAPIView(generics.ListAPIView):
 
 
 product_list_view = ProductListAPIView.as_view()
+
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+    if method == "GET":
+        if pk is not None:
+            # queryset = Product.objects.filter(pk=pk)
+            # if not queryset.exit():
+            #     raise Http404
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+
+            data = serializer.data
+            print("data: ", data)
+            return Response(data)
+        return Response({"invalid": "not good data"}, status=400)
