416221ab67614305526223d08c08d6eb38688f28
PhoebeCTY <phoebechen1996@gmail.com>
2022-07-24 15:42:42 -0400

    add pages

--- backend/sopho/urls.py
+++ backend/sopho/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('bigmelon/changefruit', views.ChangeFruitView.as_view()),
-    path('bigmelon/gameover', views.GameOverView.as_view())
+    path('bigmelon/gameover', views.GameOverView.as_view()),
+    path('bigmelon/nono', views.GameOverNoNoView.as_view(), name='nono'),
+    path('bigmelon/order', views.GameOverOrderView.as_view(), name='order'),
 ]
--- backend/sopho/views.py
+++ backend/sopho/views.py
@@ -1,5 +1,5 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 
@@ -13,5 +13,21 @@ class ChangeFruitView(View):
 class GameOverView(View):
     def get(self, request):
         win = request.GET.get('win')
+        ordered = False
+        if win == 'false' and not ordered:
+            return redirect('nono')
+        if win == 'true' and not ordered:
+            return redirect('order')
+
         context = {'win': win == 'true'}
         return render(request, 'bigmelon/gameover.html', context=context)
+
+
+class GameOverNoNoView(View):
+    def get(self, request):
+        return render(request, 'bigmelon/nono.html')
+
+
+class GameOverOrderView(View):
+    def get(self, request):
+        return render(request, 'bigmelon/order.html')
